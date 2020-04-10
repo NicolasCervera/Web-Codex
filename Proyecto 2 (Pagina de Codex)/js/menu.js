ff87c62eba037de9a8009e79c914bb1259220da9
nicolas199821 <62413542+nicolas199821@users.noreply.github.com>
2020-04-09 23:45:20 -0300

    update

--- Proyecto 2 (Pagina de Codex)/js/menu.js
+++ Proyecto 2 (Pagina de Codex)/js/menu.js
@@ -1,13 +1,16 @@
 jQuery('document').ready(function($){
 
-     var menubtn = $('.ver-menu');
+     var menubtn = $('.ver-menu'),
           menu = $('.navigation ul');
 
           menubtn.click(function(){
-               if(menu.hasClass('.show')){
-                    menu.removeClass('.show');
-               }else{
-                    menu.addClass('.show');
+               if(menu.hasClass('show'))
+               {
+                    menu.removeClass('show');
+               }
+               else
+               {
+                    menu.addClass('show');
                }
           });
 });
